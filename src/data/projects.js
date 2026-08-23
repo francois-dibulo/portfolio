a69675bf5af86ada41aea407aaa02c1b0cb9c09e
Francois <f.weberdev@gmail.com>
2026-08-23 15:00:24 +0200

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ export const projects = [
     year: 2025,
     url: 'https://cloudstacktutor.web.app/',
     shortDescription: 'A hands-on simulator for mastering Google Cloud architectures.',
-    description: 'I have spent over a decade building production systems on Google Cloud, mostly learning on the job and only ever touching a fraction of the platform. While preparing for the Cloud Engineer certification, I realized how hard it is to truly remember dozens of services or understand when I would use them and how they fit together in real scenarios. So I built CloudTutor, an interactive learning simulator that explains the most widely used Google Cloud services and challenges you to design common architectures step by step.',
+    description: 'After more than a decade building production systems on Google Cloud, I know that cloud expertise is often deep but narrow: you become highly proficient with the services your projects depend on, while an ecosystem of hundreds of other capabilities remains difficult to explore in context. While preparing for the Google Cloud Engineer certification, I decided to build a better way to learn. CloudTutor is an interactive learning simulator that introduces widely used Google Cloud services through realistic scenarios and challenges users to design common architectures step by step',
     techStack: ['JavaScript', 'VueJs', 'VueFlow', 'Tailwind', 'DaisyUI', 'Firebase'],
     images: [
       '/assets/projects/cloudtutor.webp',
@@ -33,7 +33,7 @@ export const projects = [
     year: 2024,
     url: 'https://upit.com',
     shortDescription: 'Building a conversational AI game-creation platform.',
-    description: 'I joined the UpIt (FRVR) founding team to help build a platform where anyone can create games directly in the browser using natural conversation. The system translates user intent into actual games, including code, logic, sound effects, music, and graphics, all powered by generative AI, agents and retrieval. Working closely with the founder and CTO of FRVR, I helped shape the early architecture, roadmap and real-time editor that makes game creation possible.',
+    description: 'I joined the UpIt (FRVR) founding team to help build a platform where anyone can create games directly in the browser using natural conversation. The system translates user intent into actual games, including code, logic, sound effects, music, and graphics, all powered by generative AI, agents and retrieval. Working closely with the founder and CTO of FRVR, I helped shape the early architecture, roadmap and real-time editor that makes game creation possible',
     techStack: ['JS', 'SvelteKit', 'NodeJs', 'WebSockets', 'MongoDB', 'AI', 'RAG', 'Agents'],
     images: [
       '/assets/projects/upit.webp'
@@ -47,7 +47,7 @@ export const projects = [
     year: 2023,
     url: 'https://dibulo.com',
     shortDescription: 'An interactive coloring session for kids.',
-    description: 'Dibulo turns childrens hand-colored paper templates into animated characters inside themed worlds (Sealand, Train Express, Space, and more). I had the idea already for 10 years in my mind and then started the execution during an accelerator program in Madeira, where I validated the concept with dozens of experts and parents. Dibulo has since been used in numerous kindergartens, with session lengths averaging 45 minutes. I also published several companion coloring books on Amazon, including a best-seller on Amazon.nl. The product spans web, computer vision, real-time interactions, and a full backend ecosystem.',
+    description: 'Dibulo turns childrens hand-colored paper templates into animated characters inside themed worlds (Sealand, Train Express, Space, and more). I had the idea already for 10 years in my mind and then started the execution during an accelerator program in Madeira, where I validated the concept with dozens of experts and parents. Dibulo has since been used in numerous kindergartens, with session lengths averaging 45 minutes. I also published several companion coloring books on Amazon, including a best-seller on Amazon.nl. The product spans web, computer vision, real-time interactions, and a full backend ecosystem',
     techStack: ['JavaScript', 'TypeScript', 'VueJs', 'Tailwind', 'DaisyUI', 'Firebase', 'NodeJS', 'Google Cloud', 'FastAPI', 'Python', 'OpenCV', 'Fly.io', 'Phaser'],
     images: [
       '/assets/projects/dibulo.webp',
@@ -62,7 +62,7 @@ export const projects = [
     url: 'https://airconsole.com',
     state: 'Acquired by KPIT in 2025',
     shortDescription: 'A cross-device multiplayer gaming platform turning phones into controllers.',
-    description: 'AirConsole is a real-time local multiplayer gaming platform where smartphones act as gamepads. I joined on day one as the founding engineer and contributed far beyond engineering: helping bring AirConsole to major OTT providers (TrueDigital, Swisscom, Telefónica, XLHome), Android TV ecosystems (Amazon, Xiaomi), and even platforms like Atari and Steam. Later, I worked closely with the partnerships team on the technical PoC that brought gaming into BMW in-car entertainment systems. AirConsole grew to 10M+ users and CHF 1M+ ARR, and was successfully acquired by KPIT Technologies. During my time I created 10 HTML5 games, which have been played by millions  players.',
+    description: 'AirConsole is a real-time local multiplayer gaming platform where smartphones act as gamepads. I joined on day one as the founding engineer and contributed far beyond engineering: helping bring AirConsole to major OTT providers (TrueDigital, Swisscom, Telefónica, XLHome), Android TV ecosystems (Amazon, Xiaomi), and even platforms like Atari and Steam. Later, I worked closely with the partnerships team on the technical PoC that brought gaming into BMW in-car entertainment systems. AirConsole grew to 10M+ users and CHF 1M+ ARR, and was successfully acquired by KPIT Technologies. During my time I created 10 HTML5 games, which have been played by millions players',
     techStack: ['HTML5', 'JavaScript', 'VueJs', 'AngularJs', 'NodeJs', 'Python', 'Google Cloud', 'WebSocket', 'Web-RTC', 'Phaser', 'PixiJs'],
     images: [
       '/assets/projects/airconsole.webp',
